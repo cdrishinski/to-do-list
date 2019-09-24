93e613a0cd38d4070e4ffa4eb7c0cef94fe75ccf
cdrishinski <cdrishinski@gmail.com>
2019-09-24 07:53:49 -0600

    adding items to list, adding unique id to each item

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 function addItem() {
-    alert("button working")
-}
+    var ul = document.getElementById("list-of-items");
+    var li = document.createElement("li");
+    var item = document.getElementById("new-item").value;
+
+    alert(item)
+
+    li.appendChild(document.createTextNode(item));
+    li.setAttribute("id", item.replace(/\s/g, '')); // added line
+    li.appendChild(document.createTextNode(" X"));
+    ul.appendChild(li);
+    alert(li.id);
+  }
+
+  function deleteItem (){
+      alert("button connected")
+  }
