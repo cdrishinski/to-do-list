3d2ad9cde2dfc0e3be2fa7235c848b1966964fe2
cdrishinski <cdrishinski@gmail.com>
2019-09-24 08:25:01 -0600

    additional things

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 function addItem() {
-    var ul = document.getElementById("list-of-items");
-    var li = document.createElement("li");
-    var item = document.getElementById("new-item").value;
-
-    alert(item)
+    let ul = document.getElementById("list-of-items");
+    let li = document.createElement("li");
+    let item = document.getElementById("new-item").value;
 
     li.appendChild(document.createTextNode(item));
     li.setAttribute("id", item.replace(/\s/g, '')); // added line
-    li.appendChild(document.createTextNode(" X"));
     ul.appendChild(li);
     alert(li.id);
+
+  }
+  
+  function addDeleteButton (){
+    let ul = document.getElementById("list-of-items");
+    let li = document.createElement("li");
+    let deleteButton = " X"
+
+    li.appendChild(document.createTextNode(deleteButton));
+    li.setAttribute("id", item.replace(/\s/g, '') + 1); 
+    ul.append(li);
   }
 
   function deleteItem (){
