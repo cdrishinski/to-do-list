71bd8212c937b5ab7ac005c7bf6c67c9001070fa
cdrishinski <cdrishinski@gmail.com>
2019-09-26 07:54:19 -0600

    adding X to delete items

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ function addItem() {
     let li = document.createElement("li");
     let item = document.getElementById("new-item").value;
 
-    li.appendChild(document.createTextNode(item + addDeleteButton()));
-    li.setAttribute("id", item.replace(/\s/g, '')); // added line
+    li.appendChild(document.createTextNode(item + addDeleteButton(ul, li, item)));
+    li.setAttribute("id", item.toLowerCase().replace(/\s/g, '')); // added line
     ul.appendChild(li);
 
   }
   
-  function addDeleteButton (){
-   
-    li.appendChild(document.createTextNode(" X"));
+  function addDeleteButton (ul, li, item){
+
+    li.appendChild(document.createTextNode("X "));
     li.setAttribute("id", item.replace(/\s/g + 1, ''));
     ul.appendChild(li)
 
