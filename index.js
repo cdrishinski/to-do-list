fa13633dfef484425c213bdd01937d09d9fa48d1
cdrishinski <cdrishinski@gmail.com>
2019-09-26 07:47:09 -0600

    trying to add delete button to list items

--- index.js
+++ index.js
@@ -3,21 +3,19 @@ function addItem() {
     let li = document.createElement("li");
     let item = document.getElementById("new-item").value;
 
-    li.appendChild(document.createTextNode(item));
+    li.appendChild(document.createTextNode(item + addDeleteButton()));
     li.setAttribute("id", item.replace(/\s/g, '')); // added line
     ul.appendChild(li);
-    alert(li.id);
 
   }
   
   function addDeleteButton (){
-    let ul = document.getElementById("list-of-items");
-    let li = document.createElement("li");
-    let deleteButton = " X"
+   
+    li.appendChild(document.createTextNode(" X"));
+    li.setAttribute("id", item.replace(/\s/g + 1, ''));
+    ul.appendChild(li)
 
-    li.appendChild(document.createTextNode(deleteButton));
-    li.setAttribute("id", item.replace(/\s/g, '') + 1); 
-    ul.append(li);
+   
   }
 
   function deleteItem (){
